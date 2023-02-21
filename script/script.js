553510bb66320e34976cf7c84bc04d1911716382
shumalov0 <111644866+shumalov0@users.noreply.github.com>
2023-02-21 23:53:00 +0400

    21.02.2023 anonuncment site develop

--- script/script.js
+++ script/script.js
@@ -3,6 +3,13 @@
 let icon = document.querySelector("#pasword-icon");
 let pasword = document.querySelector("#password");
 let paswordTwo = document.querySelector("#password-two");
+let paswordThree = document.querySelector("#password-three");
+let paswordFour = document.querySelector("#password-four");
+let paswordFive = document.querySelector("#password-five");
+let paswordSixt = document.querySelector("#password-sixt");
+let dropdownBtn = document.querySelector(".dropdown-btn");
+
+
 
 
 function showHide() {
@@ -18,6 +25,34 @@ function showHidetwo() {
     paswordTwo.setAttribute("type", "text");
   } else {
     paswordTwo.setAttribute("type", "password");
+  }
+}
+function showHidethree() {
+  if (paswordThree.type === "password") {
+    paswordThree.setAttribute("type", "text");
+  } else {
+    paswordThree.setAttribute("type", "password");
+  }
+}
+function showHidefour() {
+  if ( paswordFour.type === "password") {
+    paswordFour.setAttribute("type", "text");
+  } else {
+    paswordFour.setAttribute("type", "password");
+  }
+}
+function showHidefive() {
+  if ( paswordFive.type === "password") {
+    paswordFive.setAttribute("type", "text");
+  } else {
+    paswordFive.setAttribute("type", "password");
+  }
+}
+function showHidesixt() {
+  if ( paswordSixt.type === "password") {
+    paswordSixt.setAttribute("type", "text");
+  } else {
+    paswordSixt.setAttribute("type", "password");
   }
 }
 
